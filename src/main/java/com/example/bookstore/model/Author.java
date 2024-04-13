78cc5a09c67e213d19fbf580fa203007d3432601
sfatsha <f.sharifian@sinacomsys.com>
2024-04-13 19:31:23 +0330

    Override basic methods

--- src/main/java/com/example/bookstore/model/Author.java
+++ src/main/java/com/example/bookstore/model/Author.java
@@ -2,6 +2,7 @@ package com.example.bookstore.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -51,5 +52,28 @@ public class Author {
     public Author setBooks(Set<Book> books) {
         this.books = books;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -2,6 +2,7 @@ package com.example.bookstore.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -65,5 +66,29 @@ public class Book {
     public Book setAuthors(Set<Author> authors) {
         this.authors = authors;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher=" + publisher +
+                ", authors=" + authors +
+                '}';
     }
 }
