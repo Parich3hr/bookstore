1b86aaefe5980b663c06a565ecdfe39b34a1e81a
sfatsha <f.sharifian@sinacomsys.com>
2024-04-13 19:09:02 +0330

    Add project basic files

---
+++ src/main/java/com/example/bookstore/BookStoreWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreWebAppApplication.class, args);
+	}
+}
